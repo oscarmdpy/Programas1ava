51f8f1a3db041d162a0a094dc7936e42048641ad
oscarmdpy <71645357+oscarmdpy@users.noreply.github.com>
2020-09-22 13:23:05 +0200

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+print ("Donam la base del teu triangle")
+entrada1=input()
+base=int (entrada1)
+print ("Donam la seva Altura")
+entrada2=input()
+altu=int (entrada2)
+print ("La seva area es:",base*altu)
+
