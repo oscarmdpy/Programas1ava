eb71e60b2dda1f9dd505de5da2930f16a56d945e
oscarmdpy <71645357+oscarmdpy@users.noreply.github.com>
2020-09-29 13:18:25 +0200

    Add files via upload

---
+++ IVA1.py
@@ -0,0 +1,5 @@
+preuproducte = int(input("quin es el preu del producte\n"))
+print("hi haura un IVA de:",21/100*preuproducte,"€")
+print("el preu total es de",21/100*preuproducte+preuproducte,"€")
+
+
