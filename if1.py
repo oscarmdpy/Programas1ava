c99e8c6ad536aae677ea2b9bbed18b16a0efb6b5
oscarmdpy <71645357+oscarmdpy@users.noreply.github.com>
2020-10-01 12:57:34 +0200

    Add files via upload

---
+++ if1.py
@@ -0,0 +1,8 @@
+nombre1 = int(input("Primer nombre\n"))
+nombre2 = int(input("Segon nombre\n"))
+if nombre1 > nombre2:
+	print("el nombre",nombre1,"es més gran que",nombre2)
+elif nombre2 > nombre1:
+	print("el nombre",nombre2,"es mes gran que",nombre1)
+else: 
+	print("Tots dos son el nombre",nombre1)
