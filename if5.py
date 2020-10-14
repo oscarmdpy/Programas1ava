af22b051659daba579b73ace2f6c270be372a53e
oscarmdpy <71645357+oscarmdpy@users.noreply.github.com>
2020-10-14 12:23:06 +0200

    Add files via upload

---
+++ if5.py
@@ -0,0 +1,5 @@
+simbol  = int(input("escriu un simbol\n"))
+if simbol < 0: print("es un digit")
+elif simbol > 0: print("es un digit")
+elif simbol == 0: print("es un digit")
+else: print("no es un digit")
