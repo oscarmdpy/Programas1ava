706eba7d8f2db1eab5a3702d3ba6e75b7a8b766c
oscarmdpy <71645357+oscarmdpy@users.noreply.github.com>
2020-10-15 13:32:51 +0200

    Add files via upload

---
+++ if7.py
@@ -0,0 +1,11 @@
+from math import sqrt
+A = int(input("Nombre elevat a 2\n"))
+B = int(input("Nombre elevat a 1\n"))
+C = int(input("Terme independent\n"))
+if ((B**2)-4*A*C) < 0:
+	print("La rael dona negativa")
+else:
+	print("les solucions són")
+	print(-(B)+sqrt(B**2-4*A*C)/2*A)
+	print(-(B)-sqrt(B**2-4*A*C)/2*A)
+
