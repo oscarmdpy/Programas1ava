022f3c395213a41771a525a55e74b4805b5319b0
oscarmdpy <71645357+oscarmdpy@users.noreply.github.com>
2020-10-19 14:03:33 +0200

    Add files via upload

---
+++ if8.py
@@ -0,0 +1,11 @@
+dia = int(input("Diquem el dia\n"))
+mes = int(input("Diguem el mes\n"))
+any = int(input("Diguem l'any\n"))
+if dia>=30:
+	print("aquesta data es incorrecta,hi han mes de 30 dies")
+elif mes>=12:
+	print("aquesta data es incorrecta, no hi han mes de 12 mesos")
+elif any>=2020:
+	print("encara no hem arribat a aquest any")
+else:
+	print(dia,"/",mes,"/",any,"es correcta")
